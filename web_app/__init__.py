b9fc720968f3509d2686e18072c204a5ab5c7842
medamer <edamer.mo@gmail.com>
2020-06-17 06:34:52 -0700

    first try

---
+++ web_app/__init__.py
@@ -0,0 +1,25 @@
+# web_app/__init__.py
+
+from flask import Flask
+from web_app.models import db, migrate
+from web_app.routes.home_routes import home_routes
+from web_app.routes.twitter_routes import twitter_routes
+
+DATABASE_URI = "sqlite:///twitoff.db"
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(home_routes)
+    app.register_blueprint(twitter_routes)
+
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
---
+++ web_app/routes/twitter_routes.py
@@ -0,0 +1,61 @@
+# web_app/routes/twitter_routes.py
+
+from flask import Blueprint, render_template, jsonify
+from web_app.models import db, User, Tweet
+from web_app.services.twitter_service import twitter_api  #twitter_api_client
+from web_app.services.basilica_service import basilica_api_client
+
+twitter_routes = Blueprint("twitter_routes", __name__)
+
+@twitter_routes.route("/users/<screen_name>")
+def fetch_user(screen_name=None):
+    print(screen_name)
+
+    api = twitter_api    #twitter_api_client()
+
+    twitter_user = api.get_user(screen_name)
+    #tweets = api.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
+    #print("Tweets Count: ", len(tweets))
+    #return jsonify({"user": user._json, "tweets": [s._json for s in statuses]})
+
+    # Get user from database if exists, if not initialaize new one:
+    db_user = User.query.get(twitter_user.id) or User(id=twitter_user.id)
+
+    db_user.screen_name = twitter_user.screen_name
+    db_user.name = twitter_user.name
+    db_user.location = twitter_user.location
+    db_user.followers_count = twitter_user.followers_count
+
+    # store user in a database:
+    db.session.add(db_user)
+    db.session.commit()
+    
+    # Get tweets:
+    tweets = api.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
+
+    all_tweet_texts = [status.full_text for status in tweets]
+    embeddings = list(basilica_api_client.embed_sentences(all_tweet_texts, model="twitter"))
+    print("Number Of Embeddings", len(embeddings))
+
+    for index, status in enumerate(tweets):
+        print(index)
+        print(status.full_text)
+        print("----")
+
+        embeddings = embeddings[index]
+
+        #embedding = basilica_api_client.embed_sentence(status.full_text, model="twitter") 
+
+        # get existing tweet from the db or initialize a new one:
+        db_tweet = Tweet.query.get(status.id) or Tweet(id=status.id)
+
+        db_tweet.user_id = status.author.id # or db_user.id
+        db_tweet.full_text = status.full_text
+
+        #embedding = embeddings[counter]
+        print(len(embedding))
+        db_tweet.embedding = embedding
+        db.session.add(db_tweet)
+        
+    db.session.commit()
+    return "OK"
