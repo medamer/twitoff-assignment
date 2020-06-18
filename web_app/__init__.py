4590e862e0c0c4d55c0fdbf1affd94b13ab6e9e0
medamer <edamer.mo@gmail.com>
2020-06-18 10:45:54 -0700

    another try

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -8,12 +8,12 @@ from web_app.routes.home_routes import home_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
-DATABASE_URI = os.getenv("DATABASE_URI")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
